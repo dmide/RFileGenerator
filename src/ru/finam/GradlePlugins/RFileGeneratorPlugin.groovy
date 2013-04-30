382a74da471763dbf340dedc5a2be3b4b3527472
Boris Aksenov <aksenov@corp.finam.ru>
2013-04-30 22:03:49 +0400

    ready to use

--- src/ru/finam/GradlePlugins/RFileGeneratorPlugin.groovy
+++ src/ru/finam/GradlePlugins/RFileGeneratorPlugin.groovy
@@ -2,9 +2,9 @@ package ru.finam.GradlePlugins
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.UnknownDomainObjectException
 import org.gradle.api.artifacts.ProjectDependency
-import org.gradle.api.tasks.Exec
+import org.gradle.api.logging.Logger
+import org.gradle.api.logging.Logging
 
 /**
  * User: aksenov
@@ -19,57 +19,66 @@ class RFileGeneratorPluginExtension {
     String androidName = 'android'
     String androidVersion = '4.1.1.4'
 
+
 }
 
 class RFileGeneratorPlugin implements Plugin<Project> {
+    Logger logger = Logging.getLogger(Class)
+    RFileGeneratorPluginExtension extension
+
     @Override
     void apply(Project p) {
         def vars = p.extensions.create("RFileGenerator", RFileGeneratorPluginExtension)
+        extension = vars;
+        def dirs
         if (System.env.ANDROID_HOME == null) {
             throw new IllegalStateException("System variable ANDROID_HOME not found")
         }
-        p.dependencies {
-            provided "$vars.androidPackage:$vars.androidName:$vars.androidVersion"
+
+        p.gradle.taskGraph.whenReady { taskGraph ->
+            p.dependencies {
+                provided "$vars.androidPackage:$vars.androidName:$vars.androidVersion"
+            }
+            p.sourceSets {
+                main {
+                    java {
+                        srcDir vars.gen
+                    }
+                }
+            }
+            dirs = getResDirs(p)
+            logger.info('Resource dirs {}', dirs)
         }
-        p.sourceSets {
-            main {
-                java {
-                    srcDir vars.gen
+
+        p.compileJava {
+            doFirst {
+                p.exec {
+                    def params = [
+                            'package', '-v', '-f', '-m',
+                            '--auto-add-overlay',
+                            "-J", p.file(vars.gen),
+                            "-M", p.file(vars.manifest),
+                            "-I", p.configurations.provided.find { it.name == "$vars.androidName-${vars.androidVersion}.jar" }
+                    ]
+
+                    dirs.each {
+                        params.add("-S")
+                        params.add(it)
+                        logger.debug('add resource dir {}', it)
+                    }
+
+                    executable = "$System.env.ANDROID_HOME/platform-tools/aapt"
+
+                    args = params
+
+                    standardOutput = new ByteArrayOutputStream()
+
+                    ext.output = {
+                        println standardOutput.toString()
+                    }
                 }
             }
         }
-
-
-
-
-        p.task('generateRFile', type: Exec) {
-            def params = [
-                    'package', '-v', '-f', '-m',
-                    '--auto-add-overlay',
-                    "-J", p.file(vars.gen),
-                    "-M", p.file(vars.manifest),
-                    "-I", p.configurations.provided.find { it.name == "$vars.androidName-${vars.androidVersion}.jar" }
-            ]
-
-            getResDirs(p).each {
-                params.add("-S")
-                params.add(it)
-
-                println("add resource dir $it")
-            }
-
-            executable = "$System.env.ANDROID_HOME/platform-tools/aapt"
-
-            args = params
-
-            standardOutput = new ByteArrayOutputStream()
-
-            ext.output = {
-                println standardOutput.toString()
-            }
-        }
-
-        p.compileJava.dependsOn p.generateRFile
     }
 
     Set<File> getResDirs(Project p) {
@@ -80,13 +89,14 @@ class RFileGeneratorPlugin implements Plugin<Project> {
 
 
     void fillResources(Set<File> resources, Project p) {
-        println "fill dependency $p"
+        logger.debug('fill dependency {}', p)
         def resourceFile = getProjectResources(p)
+        logger.debug("retrieved res path {}", resourceFile)
         if (resourceFile != null) {
             resources.add(resourceFile)
         }
         p.configurations.compile.getDependencies().each { dep ->
-            println "check dependency $dep"
+            logger.debug('check dependency {}', dep)
             if (dep instanceof ProjectDependency) {
                 fillResources(resources, dep.getDependencyProject());
             }
@@ -94,12 +104,11 @@ class RFileGeneratorPlugin implements Plugin<Project> {
 
     }
 
-    File getProjectResources(Project project) {
-        try {
-            def ext = project.getExtensions().getByType(ru.finam.GradlePlugins.RFileGeneratorPluginExtension.class)
-            return project.file(ext.res)
-        } catch (UnknownDomainObjectException e) {
-            it.getDependencyProject()
+    static File getProjectResources(Project project) {
+        def plugin = project.getPlugins().findPlugin("RFileGenerator")
+        if (plugin != null) {
+            return project.file(plugin.extension.res)
         }
+        return null
     }
 }
